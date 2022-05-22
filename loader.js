96c8b7d56916dfe4427b17655b6017ed3e81b20b
Tyler <tylergross28@gmail.com>
2022-05-22 16:46:41 -0400

    more stuff done

--- loader.js
+++ loader.js
@@ -1,12 +1,19 @@
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const output = document.querySelector("#output");
+output.textContent = `Initializing WASM...`;
 
-await sleep(2000);
+// const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// await sleep(2000);
 
+const start = performance.now();
 const {
   instance: {
     exports: { add },
   },
 } = await WebAssembly.instantiateStreaming(fetch("./add.wasm"));
+const end = performance.now();
 
-output.textContent = `WASM Add Result: ${add(100, 200)}`;
+output.textContent = `
+WASM Loaded
+WASM add result: ${add(100, 200)}
+WASM download + parse + compile + execute time: ${(end - start).toFixed(4)}ms
+`;
